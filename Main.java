850d798f446a5013110f89f5d8a49f745781bac9
Yavor <yavor377@gmail.com>
2023-03-02 18:20:34 +0200

    msg #7 yavor maje tuk

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Maza4a tuk trq da maje");
+        System.out.println("Yavor maje");
+
+        Cat cat = new Cat("Pesho", 5);
+        Cat cat2 = new Cat("Gosho", 6);
+        System.out.println(cat.getName());
+        System.out.println(cat.getAge());
+    }
+
+}
--- maza4/src/Main.java
+++ maza4/src/Main.java
@@ -5,9 +5,9 @@ public class Main {
         System.out.println("Yavor maje");
 
         Cat cat = new Cat("Pesho", 5);
+        Cat cat2 = new Cat("Gosho", 6);
         System.out.println(cat.getName());
         System.out.println(cat.getAge());
-
     }
 
 }
