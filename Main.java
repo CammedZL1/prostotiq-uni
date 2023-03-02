85b37d772a70b730d0a496f927e41a380f4d94bb
Nikolay Zlatarov <47358059+CammedZL1@users.noreply.github.com>
2023-03-02 18:19:33 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Maza4a tuk trq da maje");
+        System.out.println("Yavor maje");
+
+        Cat cat = new Cat("Pesho", 5);
+        Cat cat = new Cat("go6o", 6);
+        System.out.println(cat.getName());
+        System.out.println(cat.getAge());
+
+    }
+
+}
