42d0b37e9686daef8d75a395005f6dd31b077600
Yavor <yavor377@gmail.com>
2023-03-02 18:13:50 +0200

    msg #5 yavor maje tuk

--- maza4/src/Main.java
+++ maza4/src/Main.java
@@ -3,6 +3,11 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Maza4a tuk trq da maje");
         System.out.println("Yavor maje");
+
+        Cat cat = new Cat("Pesho", 5);
+        System.out.println(cat.getName());
+        System.out.println(cat.getAge());
+
     }
 
 }
